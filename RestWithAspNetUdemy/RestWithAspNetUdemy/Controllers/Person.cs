5919ebc2fc5f0f991fd2f1044fecaa3aee20afd7
Guilherme Sanchez <guilhermesanchezpacheco@gmail.com>
2025-06-27 15:34:19 -0300

    continuar em casa

---
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Controllers/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithAspNetUdemy.Controllers;
+
+public class Person 
+{
+  public long Id { get; set; }
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string AddressName { get; set; }
+  public string GenderName { get; set; }
+}
+
---
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,31 @@
+﻿using RestWithAspNetUdemy.Controllers;
+
+namespace RestWithAspNetUdemy.Services.Implementations;
+
+public class PersonServiceImplementation : IPersonService {
+  public Person Create(Person person) 
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Delete(long id) 
+  {
+    throw new NotImplementedException();
+  }
+
+  public List<Person> FindAll() 
+  {
+    throw new NotImplementedException();
+  }
+
+  public Person FindById(long id) 
+  {
+    throw new NotImplementedException();
+  }
+
+  public Person Update(Person person) 
+  {
+    throw new NotImplementedException();
+  }
+}
+
